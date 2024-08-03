dd777f9e07891702c5b60db3397c637f089bb5e2
Marvin Wolfthal <maw@weichi.com>
2024-08-03 14:08:19 -0400

    line ending

--- bindings/java/build.sh
+++ bindings/java/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+RM=/usr/bin/rm
+FIND=/usr/bin/find
+SWIG=/cygdrive/d/opt/swig/bin/swig.exe
 mkdir -p src/main/java/org/rismch/verovio
-mkdir -p target
 mkdir -p target/classes/META-INF/lib
 
 # generate the git commit include file
@@ -9,32 +11,37 @@ cd ..
 ../tools/get_git_commit.sh
 cd java
 
-swig -c++ -java -package org.rismch.verovio -outdir src/main/java/org/rismch/verovio verovio.i
+cd swig
+${SWIG} -c++ -java -package org.rismch.verovio -outdir ../src/main/java/org/rismch/verovio verovio.i
+cd ..
 
-SRCFILES=$(\ls ../../src/*.cpp ../../libmei/dist/*.cpp ../../libmei/addons/*.cpp)
+#SRCFILES=$(ls ../../src/*.cpp ../../libmei/dist/*.cpp ../../libmei/addons/*.cpp)
+#ALL_FILES="$SRCFILES \
+# ../../src/pugi/pugixml.cpp \
+# ../../src/midi/Binasc.cpp \
+# ../../src/midi/MidiEvent.cpp \
+# ../../src/midi/MidiEventList.cpp \
+# ../../src/midi/MidiFile.cpp \
+# ../../src/midi/MidiMessage.cpp \
+# ../../src/hum/humlib.cpp \
+# ../../src/json/jsonxx.cc \
+# ../../src/crc/crc.cpp"
 
-FILES="$SRCFILES \
- ../../src/pugi/pugixml.cpp \
- ../../src/midi/Binasc.cpp \
- ../../src/midi/MidiEvent.cpp \
- ../../src/midi/MidiEventList.cpp \
- ../../src/midi/MidiFile.cpp \
- ../../src/midi/MidiMessage.cpp \
- ../../src/hum/humlib.cpp \
- ../../src/json/jsonxx.cc \
- ../../src/crc/crc.cpp"
+SOURCES=$(${FIND} ../../libmei/dist ../../libmei/addons ../../src  \
+-type f \( -iname \*.cc -o -iname \*.cpp \) -print0 | xargs -0 )
 
-CXXOPTS="-g -fpic -std=c++17 -I../../include -I../../include/vrv -I../../include/json -I../../include/hum -I../../include/crc -I../../include/midi -I../../include/pugi -I../../include/zip -I../../libmei/addons -I../../libmei/dist -I/opt/local/include/ "
+CXXOPTS="-g -fpic -m64 -Wa,-mbig-obj -std=c++23 \
+-I../../include -I../../include/vrv -I../../include/json \
+-I../../include/hum -I../../include/crc -I../../include/midi -I../../include/pugi \
+-I../../include/zip -I../../libmei/addons -I../../libmei/dist \
+-I/usr/java/include -I/usr/java/include/linux"
 
-PATHS=""
-unamestr=$(uname)
-if [[ "$unamestr" == 'Linux' ]]; then
-    PATHS="-I$JAVA_HOME/include -I$JAVA_HOME/include/linux " # paths to java libraries
-elif [[ "$unamestr" == 'Darwin' ]]; then
-    #JAVA_HOME="/opt/homebrew/Cellar/openjdk/20.0.1/libexec/openjdk.jdk/Contents/Home"
-    JAVA_HOME=$(/usr/libexec/java_home)
-    PATHS="-I$JAVA_HOME/include -I$JAVA_HOME/include/darwin"
-fi
+#test compilation of the wrapper
+#g++ -c $CXXOPTS verovio_wrap.cxx
 
-g++ -shared -o target/libverovio.jnilib $CXXOPTS $PATHS $FILES verovio_wrap.cxx
-cp target/libverovio.jnilib target/classes/META-INF/lib
+g++ -shared -o target/classes/META-INF/lib/libverovio.jnilib $CXXOPTS swig/verovio_wrap.cxx $SOURCES
+
+#cp target/libverovio.jnilib target/classes/META-INF/li
+${RM} ${TMPDIR}/*.o
+
+
