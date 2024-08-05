1b354e1881e035ae18851141f7bcce83da8b223b
Marvin Wolfthal <maw@weichi.com>
2024-08-04 20:09:30 -0400

    removed Weichi references
    cleanup

--- bindings/java/src/main/java/org/rismch/verovio/VrvException.java
+++ bindings/java/src/main/java/org/rismch/verovio/VrvException.java
@@ -1,26 +1,79 @@
+
 package org.rismch.verovio;
 
-import com.weichi.utils.exceptions.WeichiException;
+import org.jetbrains.annotations.NotNull;
+import org.rismch.verovio.utils.CommonUtil;
 
-public class VrvException extends WeichiException
+import java.io.Serial;
+
+public class VrvException extends Exception
 {
+    @Serial
+    private static final long serialVersionUID = 2522342728178562402L;
+
     public VrvException()
     {
         super();
     }
 
-    public VrvException( String message )
+    public VrvException( final String message )
     {
         super( message );
     }
 
-    public VrvException( String message, Throwable t )
+    public VrvException( final String message, final Throwable t )
     {
-        super( message, t );
+        super( message + ":" + t.getMessage() );
     }
 
-    public VrvException( Throwable t )
+    public VrvException( final Throwable t )
     {
-        super( t );
+        super( t.getMessage() );
+    }
+
+    /**
+     * If the argument is an VrvException just throw it as is.
+     * Otherwise, wrap it in a VrvException.
+     */
+    public static void handleException(
+        @NotNull final Throwable t ) throws VrvException
+    {
+        if ( t instanceof VrvException )
+        {
+            throw (VrvException)t;
+        }
+        else
+        {
+            throw new VrvException( t.toString() );
+        }
+    }
+
+    @Override
+    public final String toString()
+    {
+        StringBuilder sb = new StringBuilder(
+            this.getClass().getName() );
+        String msg = getMessage();
+        if ( !CommonUtil.isNullOrEmpty( msg ) )
+        {
+            sb.append( ": message: " );
+            sb.append( msg );
+        }
+
+        // try to avoid duplicate messages
+        Throwable t = this.getCause();
+        if ( null != t )
+        {
+            String causeMsg = t.getMessage();
+            if ( !CommonUtil.isNullOrEmpty( causeMsg ) &&
+                !msg.contains( causeMsg ) )
+            {
+                sb.append( ": cause: " )
+                    .append( t.getClass().getName() )
+                    .append( ": cause message: " )
+                    .append( causeMsg );
+            }
+        }
+        return sb.toString();
     }
 }
--- bindings/java/src/main/java/org/rismch/verovio/VrvRuntimeException.java
+++ bindings/java/src/main/java/org/rismch/verovio/VrvRuntimeException.java
@@ -1,14 +1,23 @@
+/*
+ * Copyright (c) 2005 - 2021 Marvin Wolfthal. All Rights Reserved.
+ */
 package org.rismch.verovio;
 
-import com.weichi.utils.exceptions.WeichiRuntimeException;
+import org.jetbrains.annotations.NotNull;
+import org.rismch.verovio.utils.CommonUtil;
 
-public class VrvRuntimeException extends WeichiRuntimeException
+import java.io.Serial;
+
+/**
+ * @author Marvin Wolfthal
+ * @since 9/12/2011
+ */
+public class VrvRuntimeException extends RuntimeException
 {
     public VrvRuntimeException()
     {
         super();
     }
-
     public VrvRuntimeException( String message )
     {
         super( message );
@@ -16,11 +25,58 @@ public class VrvRuntimeException extends WeichiRuntimeException
 
     public VrvRuntimeException( String message, Throwable t )
     {
-        super( message, t );
+        super( message + ":" + t.getMessage() );
     }
 
     public VrvRuntimeException( Throwable t )
     {
-        super( t );
+        super( t.getMessage() );
+    }
+
+    /**
+     * If the argument is a VrvException just throw it as is.
+     * Otherwise, wrap it in a VrvRuntimeException.
+     */
+    public static void handleException(
+        @NotNull final Throwable t ) throws Exception
+    {
+        CommonUtil.notNull( t, "Throwable" );
+        if ( t instanceof VrvRuntimeException )
+        {
+            throw (VrvRuntimeException)t;
+        }
+        else
+        {
+            throw new VrvRuntimeException( t );
+        }
+    }
+
+    @Override
+    public final String toString()
+    {
+        StringBuilder sb = new StringBuilder(
+            this.getClass().getName() );
+        String msg = getMessage();
+        if ( !CommonUtil.isNullOrEmpty( msg ) )
+        {
+            sb.append( ": message: " );
+            sb.append( msg );
+        }
+
+        // try to avoid duplicate messages
+        Throwable t = this.getCause();
+        if ( null != t )
+        {
+            String causeMsg = t.getMessage();
+            if ( !CommonUtil.isNullOrEmpty( causeMsg ) &&
+                !msg.contains( causeMsg ) )
+            {
+                sb.append( ": cause: " )
+                    .append( t.getClass().getName() )
+                    .append( ": cause message: " )
+                    .append( causeMsg );
+            }
+        }
+        return sb.toString();
     }
 }
