00845e95372d3b955892fe2ba2ddc82f8c536927
Marvin Wolfthal <maw@weichi.com>
2024-08-08 19:13:08 -0400

    added testNull()

--- bindings/java/src/test/java/org/rismch/verovio/test/ToolkitInitializerTest.java
+++ bindings/java/src/test/java/org/rismch/verovio/test/ToolkitInitializerTest.java
@@ -2,7 +2,10 @@ package org.rismch.verovio.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.rismch.verovio.Constants;
 import org.rismch.verovio.ToolkitInitializer;
+
+import javax.annotation.processing.SupportedSourceVersion;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +24,19 @@ public class ToolkitInitializerTest
                   e.getMessage() );
         }
         System.out.println( testInfo.getDisplayName() + " succeeded " );
+    }
+
+    @Test
+    public void testNull( TestInfo testInfo )
+    {
+        System.setProperty( Constants.PN_VEROVIO_SO_DIR, "" );
+        try
+        {
+            ToolkitInitializer.initialize();
+        }
+        catch ( Exception e )
+        {
+            System.out.println( "got unexpected exception: " + e.getMessage() );
+        }
     }
 }
