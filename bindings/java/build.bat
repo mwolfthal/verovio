464752488e8dd8c35c4853d58e2a9de4c53dc898
Marvin Wolfthal <maw@weichi.com>
2024-09-03 19:00:03 -0400

    reorg

--- bindings/java/build.bat
+++ bindings/java/build.bat
@@ -1,87 +1,85 @@
 @echo off
 setlocal
 
-rem ============================================
-rem set the repo
-rem ============================================
-@set VEROVIO_SOURCE=D:\GitHub\mwolfthal\verovio
-@set ERROR_CODE=0
-rem ============================================
-rem values to set for environment
-rem shoudn't need to change anything else
-rem ============================================
-if "X%VEROVIO_SOURCE%" == "X" (
-    @echo !! VEROVIO_SOURCE is not set !!
-    @set ERROR_CODE=1
-    goto exit)
-@echo -- VEROVIO_SOURCE is %VEROVIO_SOURCE% --
-
-@set SWIG_EXE=D:\opt\swig\bin\swig.exe
-@set VCVARS_CMD=e:\bin\vcvars64_2022
-@set CMAKE_EXE="C:\Program Files\CMake\bin\cmake.exe"
-
-rem ============================================
+rem ===============================================
 rem system tools
-rem ============================================
+rem ===============================================
 @set WHERE=where.exe
 @set ECHO=echo
 @set PUSHD=pushd
 @set POPD=popd
+@set ERRORLEVEL=0
 
-rem ============================================
-rem root paths
-rem ============================================
+rem ===============================================
+rem values to be defined for the local environment
+rem ===============================================
+
+rem ===============================================
+rem the source repository
+rem ===============================================
+@set VEROVIO_SOURCE=D:\GitHub\mwolfthal\verovio
+@set VEROVIO_SOURCE_ERROR=1
+
+if "X%VEROVIO_SOURCE%" == "X" (
+    @echo !! VEROVIO_SOURCE is not set !!
+    @set ERRORLEVEL=%VEROVIO_SOURCE_ERROR%
+    goto exit)
+@echo -- VEROVIO_SOURCE is %VEROVIO_SOURCE% --
 @set VEROVIO_JAVA_HOME=%VEROVIO_SOURCE%\bindings\java
+
+rem ===============================================
+rem java
+rem if not in the environment
+rem @set JAVA_HOME=
+rem ===============================================
+@set JAVA=%JAVA_HOME%\bin\java.exe
+
+rem ===============================================
+rem maven
+rem if not in the environment
+rem @set MAVEN_HOME=
+rem or set MAVEN to %VEROVIO_JAVA_HOME%\mvnw.cmd
+rem ===============================================
+@set MAVEN=%MAVEN_HOME%\bin\mvn.cmd
+
+rem ===============================================
+rem swig code generator
+rem ===============================================
+@set SWIG_EXE=D:\opt\swig\bin\swig.exe
+
+rem ===============================================
+rem CMake generator - nmake or Visual Studio
+rem VCVARS batch file sets the environment for nmake
+rem The one included here is for Visual Studio 2022
+rem ===============================================
+@set VCVARS_CMD=%VEROVIO_JAVA_HOME%\vcvars64_2022.bat
+@set CMAKE_EXE="C:\Program Files\CMake\bin\cmake.exe"
+@set CMAKE_GENERATOR="NMake Makefiles"
+
+rem ===============================================
+rem shouldn't need to change anything below
+rem ===============================================
+
+rem ===============================================
+rem paths based on the above
+rem ===============================================
 @set BUILD_DIR=%VEROVIO_JAVA_HOME%\build
 @set LIB_DEST_DIR=%VEROVIO_JAVA_HOME%\lib
-
-@echo -- Setting up ---
-rem ============================================
-rem find mvn or use mvnw
-rem ============================================
-@set MAVEN=mvn.cmd
-@%WHERE% /q %MAVEN%
-IF ERRORLEVEL 1 (
-    @echo !! %MAVEN% is not in the PATH, exiting. !!
-    @set ERRORCODE=2
-    goto exit)
-%ECHO% -- Found %MAVEN% --
-
-rem ============================================
-rem java
-rem ============================================
-@set JAVA=java.exe
-@%WHERE% /q %JAVA%
-IF ERRORLEVEL 1 (
-    %ECHO% !! %JAVA% is not in the PATH, exiting. !!
-    @set ERRORCODE=3
-    goto exit)
-%ECHO% -- Found %JAVA% --
-)
 @set JAVA_GENERATED_SOURCE_DIR=%VEROVIO_JAVA_HOME%\src\main\java\org\rismch\verovio\generated
 @set JAVA_GENERATED_PACKAGE=org.rismch.verovio.generated
-
-rem ============================================
-rem code generation with swig
-rem ============================================
+@set CMAKE_CONFIG_DIR=%VEROVIO_JAVA_HOME%\cmake
 @set SWIG_CONFIG_DIR=%VEROVIO_JAVA_HOME%\swig
 @set SWIG_CONFIG_FILE=%SWIG_CONFIG_DIR%\verovio.i
 @set VEROVIO_JNI_WRAPPER=%SWIG_CONFIG_DIR%\verovio_wrap.cxx
 
-rem ============================================
-rem library build definition
-rem ============================================
-@set CMAKE_CONFIG_DIR=%VEROVIO_JAVA_HOME%\cmake
-@set CMAKE_GENERATOR="NMake Makefiles"
-
-rem ============================================
+rem ===============================================
 rem just print the variables
 if "%1"=="variables" goto variables
-rem ============================================
+rem ===============================================
 
-rem ============================================
+rem ===============================================
 rem pre-build setup
-rem ============================================
+rem ===============================================
 if not exist %BUILD_DIR% (
     mkdir %BUILD_DIR%
     ) else (
@@ -96,61 +94,62 @@ del /Q %LIB_DEST_DIR%\*.*
 del /Q %JAVA_GENERATED_SOURCE_DIR%\*.java
 call %VCVARS_CMD%
 
-rem ============================================
+rem ===============================================
 rem generate the source
-rem ============================================
+rem ===============================================
 @echo -- Running swig ---
-%PUSHD% %SWIG_CONFIG_DIR%
-%SWIG_EXE% -c++ -java -package %JAVA_GENERATED_PACKAGE% ^
+@%PUSHD% %SWIG_CONFIG_DIR%
+@%SWIG_EXE% -c++ -java -package %JAVA_GENERATED_PACKAGE% ^
     -outdir %JAVA_GENERATED_SOURCE_DIR% %SWIG_CONFIG_FILE%
-%POPD%
+@%POPD%
 
-rem ============================================
+rem ===============================================
 rem Generate the build tools.
-rem CMake paths defined in environment variables
-rem require forward slashes.
-rem ============================================
+rem CMake paths require forward slashes.
+rem ===============================================
 @echo -- Generating build configuration ---
 %CMAKE_EXE% --fresh -DVEROVIO_SOURCE=%VEROVIO_SOURCE:\=//% ^
     -DVEROVIO_JNI_WRAPPER=%VEROVIO_JNI_WRAPPER:\=//% ^
     -DLIB_DEST_DIR=%LIB_DEST_DIR:\=//% ^
     -S%CMAKE_CONFIG_DIR% -B%BUILD_DIR:\=//% -G%CMAKE_GENERATOR%
 
-rem ============================================
+rem ===============================================
 rem build the library
-rem ============================================
+rem ===============================================
 @echo -- running build ---
 %CMAKE_EXE% --build %BUILD_DIR:\=//%
 
-rem ============================================
-rem build and test the toolkit
-rem ============================================
+rem ===============================================
+rem build and test the java toolkit facade
+rem ===============================================
 @echo -- Build and test jar ---
 call %MAVEN% -f %VEROVIO_JAVA_HOME% clean package test
 goto exit
 
 :variables
-@%ECHO% ====== editable paths ======
-@%ECHO% VEROVIO_SOURCE=%VEROVIO_SOURCE%
-@%ECHO% VEROVIO_JAVA_HOME=%VEROVIO_JAVA_HOME%
-@%ECHO% BUILD_DIR=%BUILD_DIR%
-@%ECHO% CMAKE_CONFIG_DIR=%CMAKE_CONFIG_DIR%
-@%ECHO% LIB_DEST_DIR=%LIB_DEST_DIR%
-@%ECHO% SWIG_CONFIG_DIR=%SWIG_CONFIG_DIR%
-@%ECHO% ====== maven ======
-@%ECHO% MAVEN=%MAVEN%
-@%ECHO% JAVA_GENERATED_SOURCE_DIR=%JAVA_GENERATED_SOURCE_DIR%
-@%ECHO% ====== swig ======
-@%ECHO% SWIG_EXE=%SWIG_EXE%
-@%ECHO% JAVA_GENERATED_PACKAGE=%JAVA_GENERATED_PACKAGE%
-@%ECHO% SWIG_CONFIG_FILE=%SWIG_CONFIG_FILE%
-@%ECHO% VEROVIO_JNI_WRAPPER=%VEROVIO_JNI_WRAPPER%
-@%ECHO% ====== cmake ======
-@%ECHO% VCVARS_CMD=%VCVARS_CMD%
-@%ECHO% CMAKE_EXE=%CMAKE_EXE%
-@%ECHO% CMAKE_GENERATOR=%CMAKE_GENERATOR%
-
+@echo ====== paths ======
+@echo VEROVIO_SOURCE=%VEROVIO_SOURCE%
+@echo VEROVIO_JAVA_HOME=%VEROVIO_JAVA_HOME%
+@echo BUILD_DIR=%BUILD_DIR%
+@echo CMAKE_CONFIG_DIR=%CMAKE_CONFIG_DIR%
+@echo LIB_DEST_DIR=%LIB_DEST_DIR%
+@echo SWIG_CONFIG_DIR=%SWIG_CONFIG_DIR%
+@echo ====== jave ======
+@echo JAVA=%JAVA%
+@echo ====== maven ======
+@echo MAVEN=%MAVEN%
+@echo JAVA_GENERATED_SOURCE_DIR=%JAVA_GENERATED_SOURCE_DIR%
+@echo ====== swig ======
+@echo SWIG_EXE=%SWIG_EXE%
+@echo JAVA_GENERATED_PACKAGE=%JAVA_GENERATED_PACKAGE%
+@echo SWIG_CONFIG_FILE=%SWIG_CONFIG_FILE%
+@echo VEROVIO_JNI_WRAPPER=%VEROVIO_JNI_WRAPPER%
+@echo ====== cmake ======
+@echo VCVARS_CMD=%VCVARS_CMD%
+@echo CMAKE_EXE=%CMAKE_EXE%
+@echo CMAKE_GENERATOR=%CMAKE_GENERATOR%
 :exit
+echo Exiting with ERRORLEVEL %ERRORLEVEL%
+exit /b %ERRORLEVEL%
 endlocal
-exit %ERRORCODE%
 
