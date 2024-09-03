6f463baeeb55f0fda91e8d2d68e55f95382eef23
Marvin Wolfthal <maw@weichi.com>
2024-09-03 16:35:08 -0400

    sync to build.bat where possible

--- bindings/java/build.sh
+++ bindings/java/build.sh
@@ -1,55 +1,81 @@
 #! /bin/bash
 
 # ============================================
-# set the repo base
+# system tools
 # ============================================
-VEROVIO_SOURCE=
-if [ "X${VEROVIO_SOURCE}" == "X" ]; then
-  echo "!! VEROVIO_SOURCE is not set !!";
-  exit 1;
-else
-  echo -- VEROVIO_SOURCE is ${VEROVIO_SOURCE} --
-fi
-
-MAVEN=$(which mvn 2>/dev/null)
-if [ "X${MAVEN}" == "X" ]; then
-    echo "!! Maven (mvn) not found !!";
-    exit 1;
-else
-    echo -- Maven is ${MAVEN} --
-fi
-
-JAVA=$(which java  2>/dev/null)
-if [ "X{JAVA}" == "X" ]; then
-    echo "!! java not found !!";
-    exit 1;
-else
-  echo -- Java is ${JAVA}
-fi
-
-# ============================================
-# values to set for environment
-# shoudn't need to change anything else
-# NOTE: JAVA_HOME must be set in environment
-# NOTE: gcc must be in the path
-# ============================================
-SWIG=/opt/swig/bin/swig
-CMAKE=/usr/bin/cmake
-CMAKE_GENERATOR="Unix Makefiles"
+WHICH=/usr/bin/which
+ECHO=/usr/bin/echo
 PWD=/usr/bin/pwd
 MKDIR=/bin/mkdir
 RM=/bin/rm
 
+VEROVIO_ERROR=1
+JAVA_ERROR=2
+MAVEN_ERROR=3
 # ============================================
-# root paths
+# values to be defined for the environment
 # ============================================
+
+# ============================================
+# set the source repository if not in the
+# environment
+# VEROVIO_SOURCE=
+# ============================================
+if [ "X${VEROVIO_SOURCE}" == "X" ]; then
+    ${ECHO} "!! VEROVIO_SOURCE is not set !!";
+    exit ${VEROVIO_ERROR};
+else
+    ${ECHO} -- VEROVIO_SOURCE is "${VEROVIO_SOURCE}" --
+fi
 VEROVIO_JAVA_HOME=${VEROVIO_SOURCE}/bindings/java
-BUILD_DIR=${VEROVIO_JAVA_HOME}/build
-LIB_DEST_DIR=${VEROVIO_JAVA_HOME}/lib
+
+# ============================================
+# java
+# JAVA_HOME=...
+# PATH=${JAVA_HOME}/bin:${PATH}
+# ============================================
+JAVA=$(${WHICH} java 2>/dev/null)
+if [ "X{JAVA}" == "X" ]; then
+    ${ECHO} "!! java not found !!";
+    exit ${JAVA_ERROR};
+else
+    ${ECHO} -- JAVA is "${JAVA}"
+fi
 
 # ============================================
 # maven
+# If maven is not installed then comment
+# the below and set MAVEN to the wrapper
+# $VEROVIO_JAVA_HOME/mvnw
 # ============================================
+MAVEN=$(${WHICH} mvn 2>/dev/null)
+if [ "X${MAVEN}" == "X" ]; then
+    ${ECHO} "!! Maven (mvn) not found !!";
+    exit ${MAVEN_ERROR};
+else
+    ${ECHO} -- MAVEN is "${MAVEN}" --
+fi
+
+# ============================================
+# swig code generator
+# ============================================
+SWIG=/opt/swig/bin/swig
+
+# ============================================
+# cmake
+# ============================================
+CMAKE=/usr/bin/cmake
+CMAKE_GENERATOR="Unix Makefiles"
+
+# ============================================
+# shouldn't need to change anything below
+# ============================================
+
+# ============================================
+# paths based on the above
+# ============================================
+BUILD_DIR=${VEROVIO_JAVA_HOME}/build
+LIB_DEST_DIR=${VEROVIO_JAVA_HOME}/lib
 JAVA_GENERATED_SOURCE_DIR=${VEROVIO_JAVA_HOME}/src/main/java/org/rismch/verovio/generated
 JAVA_GENERATED_PACKAGE=org.rismch.verovio.generated
 
@@ -68,24 +94,25 @@ CMAKE_CONFIG_DIR=${VEROVIO_JAVA_HOME}/cmake
 # just print the variables
 if [ "$1" = "variables" ]; then
 {
-    echo ====== editable paths ======
-    echo VEROVIO_SOURCE=${VEROVIO_SOURCE}
-    echo VEROVIO_JAVA_HOME=${VEROVIO_JAVA_HOME}
-    echo BUILD_DIR=${BUILD_DIR}
-    echo CMAKE_CONFIG_DIR=${CMAKE_CONFIG_DIR}
-    echo LIB_DEST_DIR=${LIB_DEST_DIR}
-    echo SWIG_CONFIG_DIR=${SWIG_CONFIG_DIR}
-    echo ====== maven ======
-    echo MAVEN=${MAVEN}
-    echo JAVA_GENERATED_SOURCE_DIR=${JAVA_GENERATED_SOURCE_DIR}
-    echo ====== swig ======
-    echo SWIG=${SWIG}
-    echo JAVA_GENERATED_PACKAGE=${JAVA_GENERATED_PACKAGE}
-    echo SWIG_CONFIG_FILE=${SWIG_CONFIG_FILE}
-    echo VEROVIO_JNI_WRAPPER=${}VEROVIO_JNI_WRAPPER}
-    echo ====== cmake ======
-    echo CMAKE=${CMAKE}
-    echo CMAKE_GENERATOR=${CMAKE_GENERATOR}
+    ${ECHO} ====== paths ======
+    ${ECHO} VEROVIO_SOURCE="${VEROVIO_SOURCE}"
+    ${ECHO} VEROVIO_JAVA_HOME="${VEROVIO_JAVA_HOME}"
+    ${ECHO} BUILD_DIR="${BUILD_DIR}"
+    ${ECHO} LIB_DEST_DIR="${LIB_DEST_DIR}"
+    ${ECHO} SWIG_CONFIG_DIR="${SWIG_CONFIG_DIR}"
+    ${ECHO} ====== java ======
+    ${ECHO} JAVA="${JAVA}"
+    ${ECHO} ====== maven ======
+    ${ECHO} MAVEN="${MAVEN}"
+    ${ECHO} JAVA_GENERATED_SOURCE_DIR="${JAVA_GENERATED_SOURCE_DIR}"
+    ${ECHO} ====== swig ======
+    ${ECHO} SWIG=${SWIG}
+    ${ECHO} JAVA_GENERATED_PACKAGE=${JAVA_GENERATED_PACKAGE}
+    ${ECHO} SWIG_CONFIG_FILE="${SWIG_CONFIG_FILE}"
+    ${ECHO} VEROVIO_JNI_WRAPPER="${VEROVIO_JNI_WRAPPER}"
+    ${ECHO} ====== cmake ======
+    ${ECHO} CMAKE=${CMAKE}
+    ${ECHO} CMAKE_GENERATOR="${CMAKE_GENERATOR}"
     if [ "X${SAVEDDIR}" != "$(${PWD})" ]; then
         cd "${SAVEDDIR}" || exit 1;
     fi
@@ -96,12 +123,12 @@ fi
 # ============================================
 # pre-build setup
 # ============================================
-if [ ! -d $BUILD_DIR ]; then ${MKDIR} $BUILD_DIR; fi
-if [ ! -d $LIB_DEST_DIR ]; then ${MKDIR} $LIB_DEST_DIR; fi
-if [ -f $VEROVIO_JNI_WRAPPER ]; then rm -f $VEROVIO_JNI_WRAPPER; fi
+if [ ! -d ${BUILD_DIR} ]; then ${MKDIR} ${BUILD_DIR}; fi
+if [ ! -d ${LIB_DEST_DIR} ]; then ${MKDIR} ${LIB_DEST_DIR}; fi
+if [ -f ${VEROVIO_JNI_WRAPPER} ]; then ${RM} -f $VEROVIO_JNI_WRAPPER; fi
 ${MAVEN} -f ${VEROVIO_JAVA_HOME} clean
-${RM} -f $LIB_DEST_DIR/*
-${RM} -f $JAVA_GENERATED_SOURCE_DIR/*.java
+${RM} -f ${LIB_DEST_DIR}/*
+${RM} -f ${JAVA_GENERATED_SOURCE_DIR}/*.java
 
 # ============================================
 # generate the source
@@ -116,19 +143,19 @@ cd ${VEROVIO_JAVA_HOME} || exit 1
 # CMake paths set in environment variables
 # require forward slashes
 # ============================================
-${CMAKE} --fresh -DVEROVIO_SOURCE=${VEROVIO_SOURCE} \
-    -DVEROVIO_JNI_WRAPPER=${VEROVIO_JNI_WRAPPER} \
+${CMAKE} --fresh -DVEROVIO_SOURCE="${VEROVIO_SOURCE}" \
+    -DVEROVIO_JNI_WRAPPER="${VEROVIO_JNI_WRAPPER}" \
     -DLIB_DEST_DIR=${LIB_DEST_DIR} \
-    -S$CMAKE_CONFIG_DIR -B${BUILD_DIR} -G"${CMAKE_GENERATOR}"
+    -S"$CMAKE_CONFIG_DIR" -B"${BUILD_DIR}" -G"${CMAKE_GENERATOR}"
 
 # ============================================
 # build the library
 # ============================================
-$CMAKE --build $BUILD_DIR
+${CMAKE} --build "${BUILD_DIR}"
 
 # ============================================
 # build and test the toolkit
 # ============================================
-$MAVEN -f ${VEROVIO_JAVA_HOME} clean package test
+${MAVEN} -f "${VEROVIO_JAVA_HOME}" clean package test
 
 exit 0
